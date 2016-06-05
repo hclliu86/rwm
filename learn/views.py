b9f7a8576ffa2b461930b783c627ec8b677d0bba
Huichun Liu <hclliu86@gmail.com>
2016-06-05 14:27:01 +0800

    list and dict
    learn how to display list and dict on the web

--- learn/views.py
+++ learn/views.py
@@ -9,4 +9,9 @@ def index(request):
 
 def home(request):
     string_ = u"我正在学习用django编写一个放射性废物管理的网页"
-    return render(request, 'home.html', {'string_': string_})
+    TutroialList = ['放射性废物',
+                    '放射性',
+                    '废物',
+                    '管理']
+    waste_info = {'type': u"固体废物", 'activity': "2E+10 Bq"}
+    return render(request, 'home.html', {'string_': string_, 'TutorialList': TutroialList})
